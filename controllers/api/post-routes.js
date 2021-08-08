580d76911d5af1996ad2b49a0c251873a1be2a71
blanden-hub <83385242+blanden-hub@users.noreply.github.com>
2021-08-07 17:35:31 -0700

    updated routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const withAuth = require ('../../utils/auth')
+// const sequelize = require ('../../config/connection');
+const { Post } = require ('../../models');
+const { route } = require('./comment-routes');
+
+
+//creating a post
+router.post('/', withAuth, (req, res)=> {
+    Post.create ({...req.body, userId: req.session.userId
+    }).then((CommentData)=> {
+        res.json (CommentData);
+       }).catch (err=>{
+           res.json(err)
+       })
+
+})
+
+
+// updating a post
+router.put ('/:id', withAuth, (res, req)=> {
+Post.update(req.body, {
+where: {
+    id: req.params.id
+}
+}).then((CommentData)=> {
+    res.json (CommentData);
+   }).catch (err=>{
+       res.json(err)
+   })
+})
+
+
+
+// delete post
+router.delete ('/:id', withAuth, (req, res)=> {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then((CommentData)=> {
+        res.json (CommentData);
+       }).catch (err=>{
+           res.json(err)
+       })
+})
+
+
+module.exports= router;
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,81 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', async (req, res)=> {
+try {
+  const userData = await User.create(req.body);
+  req.session.save(() => {
+    req.session.userId = userData.id;
+    req.session.logged_in = true;
+    
+    res.json({ user: userData, message: 'Welcome, you are now signed up!' });
+  });
+ 
+} catch(err){
+ res.status(400).json(err);
+}
+}
+)
+
+
+
+
+//expects email &password
+router.post('/login', async (req, res) => {
+  console.log("hello")
+  try {
+    const userData = await User.findOne({ where: { username: req.body.username } });
+// if wrong email
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+// if email is password is not valid
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect user or password, please try again' });
+      return;
+    }
+// if the email and password are correct
+    req.session.save(() => {
+      req.session.userId = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+// if the login times out
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+router.put('/', withAuth, async (req, res)=> {
+try {
+const updatedUserData = await User.update(re.body, {
+  where: {
+    id: req.session.userId,
+  }
+})
+res.json(updatedUserData)
+} catch (err){
+  res.status(500).json(err.errors[0].message)
+}
+})
+
+module.exports = router;
